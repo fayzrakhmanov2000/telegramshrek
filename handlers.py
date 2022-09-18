36b2e3ec0c259c8f97adc4a7fb194389ea36fdb6
fayzrakhmanov2000 <44299771+fayzrakhmanov2000@users.noreply.github.com>
2022-09-18 19:33:16 +0100

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -74,6 +74,6 @@ def forward_to_user(update, context):
 
 def setup_dispatcher(dp):
     dp.add_handler(CommandHandler('start', start))
-    dp.add_handler(MessageHandler(Filters.chat_type.private, forward_to_chat))
     dp.add_handler(MessageHandler(Filters.chat(TELEGRAM_SUPPORT_CHAT_ID) & Filters.reply, forward_to_user))
+    dp.add_handler(MessageHandler(Filters.chat_type.private, forward_to_chat))
     return dp
